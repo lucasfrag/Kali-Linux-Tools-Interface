2e56cc5024bb7ab928fc960cc453275ae13a45e6
Lucas Fraga <ti.lucasfraga@gmail.com>
2019-06-28 17:27:32 -0300

    View update

--- reports.php
+++ reports.php
@@ -19,40 +19,53 @@
 	            </div>
             	
             	<div class="card-body">
+                <div class="row icon-examples">
+
+              <?php
+                $con = getConnectionDB() or die ("Could not connect to database.");
+                $sql = $con->prepare("SELECT name, tools, command, output, solution, dataHour FROM reports ORDER BY dataHour DESC;");
+                $sql->execute();
+
+                $resultados = $sql->fetchAll(PDO::FETCH_ASSOC);
+                // FOREACH BEGINS
+                foreach ($resultados as $resultado) {
+                  $name = $resultado['name'];
+                  $tools = $resultado['tools'];
+                  $command = $resultado['command'];
+                  $output = $resultado['output'];
+                  $solution = $resultado['solution'];
+                  $dataOld = $resultado['dataHour'];
+                  $dataHour = date ("d F Y - H:i", strtotime($dataOld));
+
+
+              ?>
 				<div class="col-xl-6 col-lg-6">
                     <div class="card card-stats zoom-effect">
                       <div class="card-body">
                         <div class="row">
                           <div class="col">
-                            <h5 class="card-title text-uppercase text-muted mb-0">Available Tools</h5>
+                            <h5 class="card-title text-uppercase text-muted mb-0"><?php echo $tools; ?></h5>
                             <span class="h3 font-weight-bold mb-0">
-
-                            	<?php
-                            		$con = getConnectionDB() or die ("Could not connect to database.");
-      					                $sql = $con->prepare("SELECT * FROM tools WHERE released = 'Yes'");
-      					                $sql->execute();
-
-                            		echo $sql->rowCount();
-                            	?>
-
-                             TOOLS</span>
+                              <?php echo $name; ?><br>
+                             </span>
+                             <br><p><b>Datetime:</b> <?php echo $dataHour; ?>hrs</p>
                           </div>
                           <div class="col-auto">
-                            <div class="icon icon-shape bg-default text-white rounded-circle shadow">
-                              <i class="ni ni-bullet-list-67"></i>
+                            <div class="icon icon-shape text-white rounded-circle shadow" style="background-image: url('<?php echo $avatar2; ?>');">
                             </div>
                           </div>
                         </div>
                         <p class="mt-3 mb-0 text-muted text-sm">
                         </p>
 
-                    <a href="tools-list.php" class="btn btn-success">Show tools list</a>
+                    <a href="tools-list.php" class="btn btn-info">+ Show details</a>
                       </div>
                     </div>
                   </div>
-					
+					<?php }           ?>
 				</div>
 			</div>
+    </div>
 	<?php
 		include("assets/includes/footer.php")
 	?>
