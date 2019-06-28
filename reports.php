c19ceab125b5f67eec51e0e3a96d4f6018df1913
Lucas Fraga <ti.lucasfraga@gmail.com>
2019-06-28 17:32:42 -0300

    Pagination view

--- reports.php
+++ reports.php
@@ -64,7 +64,28 @@
                   </div>
 					<?php }           ?>
 				</div>
-			</div>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-end">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1">
+        <i class="fa fa-angle-left"></i>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+
+    <li class="page-item">
+      <a class="page-link" href="#">
+        <i class="fa fa-angle-right"></i>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+      </div>
+
+
     </div>
 	<?php
 		include("assets/includes/footer.php")
